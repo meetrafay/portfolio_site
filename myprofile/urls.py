e40f8cbc86ce9656fc8dce2951c4ea769af78f25
Abdul Rafay <khanrafay358@gmail.com>
2022-03-10 15:16:27 +0500

    deployment files added

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import path , include
 from home import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name = 'homepage'),
     path('ski/',include('skills.urls')),
     path('edu/',include('education.urls')),
     path('con/',include('contact.urls')),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
